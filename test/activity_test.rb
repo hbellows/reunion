6c406447154a085408cf0bbbc44cb0ed04b0d2cb
Harper Bellows <harper.bellows@gmail.com>
2018-05-30 09:47:16 -0600

    initial commit

--- test/activity_test.rb
+++ test/activity_test.rb
@@ -4,4 +4,22 @@ require 'minitest/pride'
 require './lib/activity'
 
 class ActivityTest < Minitest::Test
+
+  def test_activity_has_a_name
+    activity = Activity.new('Sky Diving')
+
+    assert_equal 'Sky Diving', activity.name
+  end
+
+  def test_activity_starts_with_no_participants
+    activity = Activity.new('Alcatraz')
+
+    assert_equal [], activity.participants
+  end
+
+  def test_participants_start_with_no_bill
+    activity = Activity.new('Fun Run')
+
+    assert_equal {}, activity.bill
+  end
 end
