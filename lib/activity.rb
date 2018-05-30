b538c3aa94516805ce2f4113b50f6484cecccb5d
Harper Bellows <harper.bellows@gmail.com>
2018-05-30 09:47:04 -0600

    initial commit

--- lib/activity.rb
+++ lib/activity.rb
@@ -1,2 +1,17 @@
 class Activity
+  attr_reader :name, :participants, :bill
+
+  def initialize(activity_name)
+    @name = activity_name
+    @participants = []
+    @bill = Hash.new(0)
+  end
+
+  def total_cost
+
+  end
+
+  def cost_per_person
+
+  end
 end
