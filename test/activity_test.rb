03a45dbe764e75c0e0c9ebd8d7192f8e2b5e421c
Harper Bellows <harper.bellows@gmail.com>
2018-05-30 10:19:16 -0600

    work ongoing

--- test/activity_test.rb
+++ test/activity_test.rb
@@ -6,20 +6,20 @@ require './lib/activity'
 class ActivityTest < Minitest::Test
 
   def test_activity_has_a_name
-    activity = Activity.new('Sky Diving')
+    activity = Activity.new('Sky Diving', 1000)
 
     assert_equal 'Sky Diving', activity.name
   end
 
   def test_activity_starts_with_no_participants
-    activity = Activity.new('Alcatraz')
+    activity = Activity.new('Alcatraz', 200)
 
     assert_equal [], activity.participants
   end
 
-  def test_participants_start_with_no_bill
+  def test_participants_are_added_to_the_activity
     activity = Activity.new('Fun Run')
 
-    assert_equal {}, activity.bill
+    assert_equal ['Jane'], activity.add_participants
   end
 end
