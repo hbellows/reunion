e970e2bb3a4e920ef55deddc89cc4d4ddb9f86e4
Harper Bellows <harper.bellows@gmail.com>
2018-05-30 10:26:32 -0600

    WIP

--- lib/activity.rb
+++ lib/activity.rb
@@ -1,20 +1,20 @@
 class Activity
   attr_reader :name, :participants, :bill, :cost
 
-  def initialize(activity_name)
+  def initialize(activity_name, base_cost)
     @name = activity_name
     @cost = base_cost
     @participants = []
     @bill = Hash.new(0)
   end
 
-  def add_participants
-
+  def add_participants(person)
+    @participants << person
   end
 
-  # def total_cost
-  #
-  # end
+  def total_bill
+
+  end
   #
   # def cost_per_person
   #
