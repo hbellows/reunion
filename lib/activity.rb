5801d5828e6b5bd164264106d4efd2f0c4b2d069
Harper Bellows <harper.bellows@gmail.com>
2018-05-30 10:19:03 -0600

    work ongoing

--- lib/activity.rb
+++ lib/activity.rb
@@ -1,17 +1,22 @@
 class Activity
-  attr_reader :name, :participants, :bill
+  attr_reader :name, :participants, :bill, :cost
 
   def initialize(activity_name)
     @name = activity_name
+    @cost = base_cost
     @participants = []
     @bill = Hash.new(0)
   end
 
-  def total_cost
+  def add_participants
 
   end
 
-  def cost_per_person
-
-  end
+  # def total_cost
+  #
+  # end
+  #
+  # def cost_per_person
+  #
+  # end
 end
